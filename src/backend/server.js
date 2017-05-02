773e44c640731fb2b21eab05e247e5c179353d01
TroublemakerSt <ilya-art@mail.ru>
2017-05-02 15:30:15 +0300

    Loading animation and catch the errors

--- src/backend/server.js
+++ src/backend/server.js
@@ -12,5 +12,13 @@ mongodb.MongoClient.connect(dbUrl, function (err, db) {
     });
   });
 
+  app.use((req, res) => {
+    res.status(404).json({
+      errors: {
+        global: 'Stil working on it. Please try again later when we implement it',
+      },
+    });
+  });
+
   app.listen(8080, () => console.log('Server is running on localhost:8080'));
 });
