616f8e0c7eb5fc8450a7685ac553bfda678512ee
TroublemakerSt <ilya-art@mail.ru>
2017-05-04 22:28:24 +0300

    Next Step on Server side

--- src/components/GameForm.jsx
+++ src/components/GameForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 
 import { saveGame } from '../actions/actions';
 
@@ -9,7 +10,8 @@ class GameForm extends React.Component {
     title: '',
     cover: '',
     errors: {},
-    loading: false
+    loading: false,
+    done: false
   };
 
   handleChange = (e) => {
@@ -29,20 +31,20 @@ class GameForm extends React.Component {
     e.preventDefault();
 
     // validation
-    // let errors = {};
-    // if (this.state.title === '') errors.title = "Can't be empty";
-    // if (this.state.cover === '') errors.cover = "Can't be empty";
-    // 
-    // this.setState({ errors });
-    //
-    // const isValid = Object.keys(errors).length === 0;
+    let errors = {};
+    if (this.state.title === '') errors.title = "Can't be empty";
+    if (this.state.cover === '') errors.cover = "Can't be empty";
 
-    if (true) {
+    this.setState({ errors });
+
+    const isValid = Object.keys(errors).length === 0;
+
+    if (isValid) {
       const { title, cover } = this.state;
       this.setState({ loading: true });
       console.log(this.state.errors);
       this.props.saveGame({ title, cover }).then(
-        () => {},
+        () => { this.setState({ done: true }) },
         (err) => err.response.json().then(({errors}) => this.setState({ errors, loading: false }))
       );
       console.log(this.state.errors.global);
@@ -50,43 +52,46 @@ class GameForm extends React.Component {
   }
 
   render() {
+    const form = (
+      <form className={classnames('ui', 'form', { loading: this.state.loading })} onSubmit={this.handleSubmit}>
+        <h1>Add new game</h1>
+
+        {!!this.state.errors.global && <div className="ui negative message"><p>{this.state.errors.global}</p></div>}
+
+        <div className={classnames('field', { error: !!this.state.errors.title })}>
+          <label htmlFor="title">Title</label>
+          <input
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+            id="title"
+          />
+          <span>{this.state.errors.title}</span>
+        </div>
+
+        <div className={classnames('field', { error: !!this.state.errors.cover })}>
+          <label htmlFor="cover">Cover URL</label>
+          <input
+            name="cover"
+            value={this.state.cover}
+            onChange={this.handleChange}
+            id="cover"
+          />
+          <span>{this.state.errors.cover}</span>
+        </div>
+
+        <div className="field">
+          {this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image"/>}
+        </div>
+
+        <div className="field">
+          <button className="ui primary button">Save</button>
+        </div>
+      </form>
+    );
     return (
       <div>
-        <form className={classnames('ui', 'form', { loading: this.state.loading })} onSubmit={this.handleSubmit}>
-          <h1>Add new game</h1>
-
-          {!!this.state.errors.global && <div className="ui negative message"><p>{this.state.errors.global}</p></div>}
-
-          <div className={classnames('field', { error: !!this.state.errors.title })}>
-            <label htmlFor="title">Title</label>
-            <input
-              name="title"
-              value={this.state.title}
-              onChange={this.handleChange}
-              id="title"
-            />
-            <span>{this.state.errors.title}</span>
-          </div>
-
-          <div className={classnames('field', { error: !!this.state.errors.cover })}>
-            <label htmlFor="cover">Cover URL</label>
-            <input
-              name="cover"
-              value={this.state.cover}
-              onChange={this.handleChange}
-              id="cover"
-            />
-            <span>{this.state.errors.cover}</span>
-          </div>
-
-          <div className="field">
-            {this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image"/>}
-          </div>
-
-          <div className="field">
-            <button className="ui primary button">Save</button>
-          </div>
-        </form>
+        { this.state.done ? <Redirect to="/games" /> : form }
       </div>
     );
   }
